16cc888c9f2a8d07a86794b508981c7b45280a76
reaper <ahoy@barelyhuman.dev>
2024-12-29 01:11:30 +0530

    fix: duplication of footer

--- src/layouts/base.jsx
+++ src/layouts/base.jsx
@@ -2,7 +2,7 @@ import { Sidebar } from "../components/Sidebar";
 
 export default function BaseLayout({ children, sideBarItems = [] }) {
   return (
-    <>
+    <div>
       <div class="min-h-[80vh] mx-auto max-w-4xl p-2">
         <header class="flex flex-col justify-center h-[400px]">
           <h1 class="font-semibold text-2xl">
@@ -29,13 +29,23 @@ export default function BaseLayout({ children, sideBarItems = [] }) {
       <footer class="mx-auto border-t border-t-zinc max-w-4xl p-2">
         <ul class="flex gap-4 justify-end">
           <li class="text-xs">
-            <a class="w-full hover:underline hover:underline-offset-4" href="https://github.com/barelyhuman">Github</a>
+            <a
+              class="w-full hover:underline hover:underline-offset-4"
+              href="https://github.com/barelyhuman"
+            >
+              Github
+            </a>
           </li>
           <li class="text-xs">
-            <a class="w-full hover:underline hover:underline-offset-4" href="https://github.com/barelyhuman">X</a>
+            <a
+              class="w-full hover:underline hover:underline-offset-4"
+              href="https://github.com/barelyhuman"
+            >
+              X
+            </a>
           </li>
         </ul>
       </footer>
-    </>
+    </div>
   );
 }
