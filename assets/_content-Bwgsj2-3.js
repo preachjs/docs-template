68183bafe2fc46d6bd677e9c5f1f2e5e46b7e701
barelyhuman <sidgroks@gmail.com>
2024-12-28 19:23:21 +0000

    Deploying to gh-pages from @ preachjs/docs-template@2e78affc22161411981eb5a6d12f8736e48912a3 🚀

---
+++ assets/_content-Bwgsj2-3.js
@@ -0,0 +1,1 @@
+import{u as r,a as t,k as n}from"./main-CSW5LkW0.js";import{s as o,B as u}from"./base-BXgzCyCe.js";function m(){var e;const s=r(),a=((e=o[s.params.content])==null?void 0:e.source)||(()=>t(n,{}));return t(u,{sideBarItems:o,children:t(a,{})})}export{m as default};
---
+++ assets/about-B7nn7cBD.js
@@ -0,0 +1,9 @@
+import{a as e,k as a}from"./main-CSW5LkW0.js";function i(t){const n={a:"a",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...t.components};return e(a,{children:[e(n.h2,{children:"About"}),`
+`,e(n.p,{children:[`Minimal document template based on Vite and Preact and takes inspiration from
+`,e(n.a,{href:"https://antidote.sh",children:"antidote.sh"})]}),`
+`,e(n.h3,{children:"Features"}),`
+`,e(n.ul,{children:[`
+`,e(n.li,{children:["Based on ",e(n.a,{href:"https://vite.dev/",children:"Vite"}),", so handles basic dev tooling for you."]}),`
+`,e(n.li,{children:["Tiny layer on top of ",e(n.a,{href:"https://preactjs.com/",children:"Preact"}),", so the overall js bundle in tiny."]}),`
+`,e(n.li,{children:'Tries to be "Just Javascript" instead of magically hiding everything'}),`
+`]})]})}function r(t={}){const{wrapper:n}=t.components||{};return n?e(n,{...t,children:e(i,{...t})}):i(t)}export{r as default};
