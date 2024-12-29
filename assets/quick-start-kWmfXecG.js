2202d57a18f00c20647b967d29c34303df6baaa4
barelyhuman <sidgroks@gmail.com>
2024-12-29 06:04:10 +0000

    Deploying to gh-pages from @ preachjs/docs-template@2ac48db411e8f4ca720fea069edf93df8fe8f185 🚀

--- assets/quick-start-kWmfXecG.js
+++ assets/quick-start-kWmfXecG.js
@@ -1,4 +1,4 @@
-import{a as e,k as c}from"./main-ByFQtET_.js";function o(t){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",...t.components};return e(c,{children:[e(n.h2,{children:"Quick Start"}),`
+import{a as e,k as c}from"./main-By_pwd6M.js";function o(t){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",...t.components};return e(c,{children:[e(n.h2,{children:"Quick Start"}),`
 `,e(n.ol,{children:[`
 `,e(n.li,{children:["Clone the ",e(n.a,{href:"https://github.com/preachjs/docs-template",children:"repository"})]}),`
 `,e(n.li,{children:["Modify ",e(n.code,{children:"content/data.js"})," with the needed data"]}),`
